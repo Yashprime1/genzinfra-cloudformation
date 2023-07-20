3fde31dda5fc4eea925504c557e80d308713b151
Yashdeep Shetty <yashdeep@clevertap.com>
2023-07-21 02:01:59 +0530

    Added DS service

--- lib/components/ds/base/outputs.go
+++ lib/components/ds/base/outputs.go
@@ -9,7 +9,7 @@ func AddOutputsForDsBaseStack(template *cloudformation.Template) {
 		Description: cloudformation.String("DS ELB Target Group Arn"),
 		Value:       cloudformation.Ref("DsElbTargetGroup"),
 		Export: &cloudformation.Export{
-			Name: cloudformation.Sub("${AWS::StackName}-DsElbTargetGroupArn"),
+			Name: cloudformation.Sub("${AWS::StackName}-Service-DsElbTargetGroupArn"),
 		},
 	}
 }
