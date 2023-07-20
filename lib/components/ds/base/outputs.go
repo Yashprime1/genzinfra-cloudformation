29d3bec9e02e2d702826b24be31126c26c78ab4a
Yashdeep Shetty <yashdeep@clevertap.com>
2023-07-21 01:17:00 +0530

    Added DS base and service stack remove comments

---
+++ lib/components/ds/base/outputs.go
@@ -0,0 +1,15 @@
+package dsbase
+
+import (
+	"github.com/awslabs/goformation/v7/cloudformation"
+)
+
+func AddOutputsForDsBaseStack(template *cloudformation.Template) {
+	template.Outputs["DsElbTargetGroupArn"] = cloudformation.Output{
+		Description: cloudformation.String("DS ELB Target Group Arn"),
+		Value:       cloudformation.Ref("DsElbTargetGroup"),
+		Export: &cloudformation.Export{
+			Name: cloudformation.Sub("${AWS::StackName}-DsElbTargetGroupArn"),
+		},
+	}
+}
