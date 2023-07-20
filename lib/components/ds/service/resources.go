89da65410cd4f3fde2be995f047a3cb4664c2504
Yashdeep Shetty <yashdeep@clevertap.com>
2023-07-21 01:02:21 +0530

    Added DS base and service stack

--- lib/components/ds/service/resources.go
+++ lib/components/ds/service/resources.go
@@ -78,59 +78,59 @@ func AddResourcesForDsServiceStack(template *cloudformation.Template) {
 
 
 	template.Resources["DsEcsCluster"] = &ecs.Cluster{}
-	template.Resources["DsEcsTaskDefinition"] = &ecs.TaskDefinition{
-		NetworkMode:      cloudformation.String("host"),
-		Family:           cloudformation.String(cloudformation.Ref("AWS::StackName")),
-		ExecutionRoleArn: cloudformation.String(cloudformation.Ref("DsEcsTaskExecutionRole")),
-		ContainerDefinitions: []ecs.TaskDefinition_ContainerDefinition{
-			{
-				Name: "ds",
-				Image: "httpd",
-				Environment: []ecs.TaskDefinition_KeyValuePair{
-					{
-						Name:  cloudformation.String(""),
-						Value: cloudformation.String(""),
-					},
-				},
-				LogConfiguration: &ecs.TaskDefinition_LogConfiguration{
-					LogDriver: "awslogs",
-				},
-				Essential: cloudformation.Bool(true),
-				MemoryReservation: cloudformation.Int(300),
-				Privileged:        cloudformation.Bool(false),
-				ReadonlyRootFilesystem: cloudformation.Bool(false),
-				User:                   cloudformation.String("ds"),
-				Ulimits: []ecs.TaskDefinition_Ulimit{
-					{
-						Name:      "nofile",
-						SoftLimit: 65536,
-						HardLimit: 65536,
-					},
-				},
-			},
-		},
-	}
-	template.Resources["DsEcsService"] = &ecs.Service{
-		Cluster: cloudformation.String(cloudformation.Ref("DsEcsCluster")),
-		DeploymentConfiguration: &ecs.Service_DeploymentConfiguration{
-			MaximumPercent:        cloudformation.Int(200),
-			MinimumHealthyPercent: cloudformation.Int(50),
-		},
-		HealthCheckGracePeriodSeconds: cloudformation.Int(60),
-		LaunchType:                    cloudformation.String("EC2"),
-		LoadBalancers:                 []ecs.Service_LoadBalancer{
-			{
-				ContainerName:  cloudformation.String("ds"),
-				ContainerPort:  cloudformation.Int(80),
-				TargetGroupArn: cloudformation.String(cloudformation.ImportValue(cloudformation.Sub("${AWS::StackName}-DsElbTargetGroupArn"))),
-			},
-		},
-		PlacementConstraints: []ecs.Service_PlacementConstraint{
-			{
-				Type: "distinctInstance",
-			},
-		},
-		SchedulingStrategy: cloudformation.String("DAEMON"),
-		TaskDefinition:     cloudformation.String(cloudformation.Ref("DsEcsTaskDefinition")),
-	}
+	// template.Resources["DsEcsTaskDefinition"] = &ecs.TaskDefinition{
+	// 	NetworkMode:      cloudformation.String("host"),
+	// 	Family:           cloudformation.String(cloudformation.Ref("AWS::StackName")),
+	// 	ExecutionRoleArn: cloudformation.String(cloudformation.Ref("DsEcsTaskExecutionRole")),
+	// 	ContainerDefinitions: []ecs.TaskDefinition_ContainerDefinition{
+	// 		{
+	// 			Name: "ds",
+	// 			Image: "httpd",
+	// 			Environment: []ecs.TaskDefinition_KeyValuePair{
+	// 				{
+	// 					Name:  cloudformation.String(""),
+	// 					Value: cloudformation.String(""),
+	// 				},
+	// 			},
+	// 			LogConfiguration: &ecs.TaskDefinition_LogConfiguration{
+	// 				LogDriver: "awslogs",
+	// 			},
+	// 			Essential: cloudformation.Bool(true),
+	// 			MemoryReservation: cloudformation.Int(300),
+	// 			Privileged:        cloudformation.Bool(false),
+	// 			ReadonlyRootFilesystem: cloudformation.Bool(false),
+	// 			User:                   cloudformation.String("ds"),
+	// 			Ulimits: []ecs.TaskDefinition_Ulimit{
+	// 				{
+	// 					Name:      "nofile",
+	// 					SoftLimit: 65536,
+	// 					HardLimit: 65536,
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+	// template.Resources["DsEcsService"] = &ecs.Service{
+	// 	Cluster: cloudformation.String(cloudformation.Ref("DsEcsCluster")),
+	// 	DeploymentConfiguration: &ecs.Service_DeploymentConfiguration{
+	// 		MaximumPercent:        cloudformation.Int(200),
+	// 		MinimumHealthyPercent: cloudformation.Int(50),
+	// 	},
+	// 	HealthCheckGracePeriodSeconds: cloudformation.Int(60),
+	// 	LaunchType:                    cloudformation.String("EC2"),
+	// 	LoadBalancers:                 []ecs.Service_LoadBalancer{
+	// 		{
+	// 			ContainerName:  cloudformation.String("ds"),
+	// 			ContainerPort:  cloudformation.Int(80),
+	// 			TargetGroupArn: cloudformation.String(cloudformation.ImportValue(cloudformation.Sub("${AWS::StackName}-DsElbTargetGroupArn"))),
+	// 		},
+	// 	},
+	// 	PlacementConstraints: []ecs.Service_PlacementConstraint{
+	// 		{
+	// 			Type: "distinctInstance",
+	// 		},
+	// 	},
+	// 	SchedulingStrategy: cloudformation.String("DAEMON"),
+	// 	TaskDefinition:     cloudformation.String(cloudformation.Ref("DsEcsTaskDefinition")),
+	// }
 }
