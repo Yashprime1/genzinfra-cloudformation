8f08e5952a1dca2601b62bb462e279049ba55b47
Yashdeep Shetty <yashdeep@clevertap.com>
2023-07-21 20:54:06 +0530

    added stacks ds

--- lib/components/ds/base/parameters.go
+++ lib/components/ds/base/parameters.go
@@ -6,29 +6,29 @@ import (
 func AddParametersForDsBaseStack(template *cloudformation.Template,defaults DsBaseDefaults ){
 	// Add the parameters
 	// Add the parameters
-	// template.Parameters["DsAmiId"] = cloudformation.Parameter{
-	// 	Type: "String",
-	// 	Description: cloudformation.String("The AMI ID for the DS"),
-	// }
+	template.Parameters["DsAmiId"] = cloudformation.Parameter{
+		Type: "String",
+		Description: cloudformation.String("The AMI ID for the DS"),
+	}
 	
-	// template.Parameters["DsInstanceType"] = cloudformation.Parameter{
-	// 	Type: "String",
-	// 	Description: cloudformation.String("The instance type for the DS"),
-	// }
+	template.Parameters["DsInstanceType"] = cloudformation.Parameter{
+		Type: "String",
+		Description: cloudformation.String("The instance type for the DS"),
+	}
 	
-	// template.Parameters["DsMaxSize"] = cloudformation.Parameter{
-	// 	Type: "String",
-	// 	Description: cloudformation.String("The maximum size for the DS"),
-	// }
+	template.Parameters["DsMaxSize"] = cloudformation.Parameter{
+		Type: "String",
+		Description: cloudformation.String("The maximum size for the DS"),
+	}
 
-	// template.Parameters["DsMinSize"] = cloudformation.Parameter{
-	// 	Type: "String",
-	// 	Description: cloudformation.String("The minimum size for the DS"),
-	// }
+	template.Parameters["DsMinSize"] = cloudformation.Parameter{
+		Type: "String",
+		Description: cloudformation.String("The minimum size for the DS"),
+	}
 
-	// template.Parameters["DsDesiredSize"] = cloudformation.Parameter{
-	// 	Type: "String",
-	// 	Description: cloudformation.String("The desired capacity for the DS"),
-	// }
+	template.Parameters["DsDesiredSize"] = cloudformation.Parameter{
+		Type: "String",
+		Description: cloudformation.String("The desired capacity for the DS"),
+	}
 }
 
