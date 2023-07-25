e573ee478a6440fe7cffa1ebe4df18cb5f184666
Yashdeep Shetty <yashdeep@clevertap.com>
2023-07-25 23:55:48 +0530

    added listener and TG

--- lib/components/ds/base/resources.go
+++ lib/components/ds/base/resources.go
@@ -117,6 +117,31 @@ func AddResourcesForDsBaseStack(template *cloudformation.Template, defaults DsBa
 			},
 		},
 	}
+	template.Resources["DsPrometheusElbTargetGroup"] = &elasticloadbalancingv2.TargetGroup{
+		HealthCheckIntervalSeconds: cloudformation.Int(30),
+		HealthCheckPath:            cloudformation.String("/"),
+		HealthCheckProtocol:        cloudformation.String("HTTP"),
+		HealthCheckTimeoutSeconds:  cloudformation.Int(10),
+		HealthyThresholdCount:      cloudformation.Int(5),
+		Matcher: &elasticloadbalancingv2.TargetGroup_Matcher{
+			HttpCode: cloudformation.String("200"),
+		},
+		Port:                    cloudformation.Int(9090),
+		Protocol:                cloudformation.String("HTTP"),
+		TargetType:              cloudformation.String("instance"),
+		UnhealthyThresholdCount: cloudformation.Int(2),
+		VpcId:                   cloudformation.String(cloudformation.ImportValue(defaults.NetworkStack + "-AppVPCId")),
+		TargetGroupAttributes: []elasticloadbalancingv2.TargetGroup_TargetGroupAttribute{
+			{
+				Key:   cloudformation.String("stickiness.enabled"),
+				Value: cloudformation.String("false"),
+			},
+			{
+				Key:   cloudformation.String("deregistration_delay.timeout_seconds"),
+				Value: cloudformation.String("300"),
+			},
+		},
+	}
 	template.Resources["DsElbListener"] = &elasticloadbalancingv2.Listener{
 		DefaultActions: []elasticloadbalancingv2.Listener_Action{
 			{
@@ -126,6 +151,17 @@ func AddResourcesForDsBaseStack(template *cloudformation.Template, defaults DsBa
 		},
 		LoadBalancerArn: cloudformation.Ref("DsElb"),
 		Port:            cloudformation.Int(80),
+		Protocol:        cloudformation.String("HTTP"),
+	}
+	template.Resources["DsPrometheusElbListener"] = &elasticloadbalancingv2.Listener{
+		DefaultActions: []elasticloadbalancingv2.Listener_Action{
+			{
+				TargetGroupArn: cloudformation.String(cloudformation.Ref("DsPrometheusElbTargetGroup")),
+				Type:           "forward",
+			},
+		},
+		LoadBalancerArn: cloudformation.Ref("DsElb"),
+		Port:            cloudformation.Int(9090),
 		Protocol:        cloudformation.String("HTTP"),
 	}
 }
