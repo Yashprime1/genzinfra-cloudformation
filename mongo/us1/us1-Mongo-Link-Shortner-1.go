d1c0416f0187fa539c245eb43242ed806776a1ce
Yashdeep Shetty <yashdeep@clevertap.com>
2023-07-31 19:30:25 +0530

    Mongo :  Updating instance - 10.16.6.229
    SNE-39400, SNE-39999

--- mongo/us1/us1-Mongo-Link-Shortner-1.go
+++ mongo/us1/us1-Mongo-Link-Shortner-1.go
@@ -70,8 +70,8 @@ func GenerateUs1MongoLinkShortnerTemplate() {
 	mongoReplicaInstance006133.AppendToTemplate(sTemplate, serviceTemplate)
 
 	mongoReplicaInstance006229 := mongo.NewMongo(defaults)
-	mongoReplicaInstance006229.EnableEc2instance = false
-	mongoReplicaInstance006229.Ec2Instance.ImageId = cloudformation.String("ami-02726fee3f464392b")
+	mongoReplicaInstance006229.EnableEc2instance = true
+	mongoReplicaInstance006229.Ec2Instance.ImageId = cloudformation.String("ami-0a9d27a9f4f5c0efc")
 	mongoReplicaInstance006229.Ec2Instance.InstanceType = cloudformation.String("r5.2xlarge")
 	mongoReplicaInstance006229.Ec2InstanceSubnet = subnetB
 	mongoReplicaInstance006229.Ec2Instance.PrivateIpAddress = cloudformation.String("10.16.6.229") 
@@ -81,7 +81,7 @@ func GenerateUs1MongoLinkShortnerTemplate() {
 	mongoReplicaInstance006229.EnableMongoRegistryCache = true
 	mongoReplicaInstance006229.EnableXvdpGp3 = true
 	mongoReplicaInstance006229.XvdpEc2Volume.Iops = cloudformation.Int(3000)
-	mongoReplicaInstance006229.MongoContainerTag = "bamboo-mongo-task-2652-mongo-9"
+	mongoReplicaInstance006229.MongoContainerTag = "bamboo-mongo-task-2652-mongo-10"
 	mongoReplicaInstance006229.AppendToTemplate(sTemplate, serviceTemplate)
 
 	mongoReplicaInstance006149 := mongo.NewMongo(defaults)
