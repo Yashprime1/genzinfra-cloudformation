4bb654dc9cd082889499e17a4bacdf9512eab64b
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 15:10:33 +0530

    updating build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ echo $bamboo_deploy_environment
 echo $bamboo_buildResultKey
 python3 scripts/replace_tags.py
 git --version
-git config --global --name Yashdeep Shetty --email yashdeep@clevertap.com
+git config --global user.name "Yashdeep Shetty"
+git config --global user.email yashdeep@clevertap.com
 git commit -am "Updating tags to deploy"
 # git push --set-upstream origin master
