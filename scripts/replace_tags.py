11faae96b2e264a20a6bd38d31fff3caf4c6f966
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 12:15:52 +0530

    Adding dash and nb

--- scripts/replace_tags.py
+++ scripts/replace_tags.py
@@ -30,9 +30,9 @@ if __name__ == "__main__":
     stackname = os.getenv('bamboo_deploy_environment')
 
     if "dash" in stackname:
-        deployimage = "yashprime07/dashboard:"+  os.getenv('bamboo_buildResultKey')
+        deployimage = "yashprime07/dashboard:"+os.getenv('bamboo_buildKey')+"-"+os.getenv('bamboo_buildNumber')
     else:
-        deployimage = "yashprime07/notificationbackend:"+  os.getenv('bamboo_buildResultKey')
+        deployimage = "yashprime07/notificationbackend:"+os.getenv('bamboo_buildKey')+"-"+os.getenv('bamboo_buildNumber')
     stack_data["Stacks"][region][stackname]["Parameters"]["ContainerImage"]=deployimage
 
 
