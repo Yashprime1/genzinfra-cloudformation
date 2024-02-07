beb7cbff0001f1add53f4ac566e56220da4c6f51
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 16:08:16 +0530

    updating build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ python3 scripts/replace_tags.py
 git --version
 git config --global user.name "Yashdeep Shetty"
 git config --global user.email yashdeep@clevertap.com
-git clone https://x-access-token:$gittoken@github.com/Yashprime1/genzinfra-cloudformation.git
+git clone https://x-access-token:$bamboo_gittoken@github.com/Yashprime1/genzinfra-cloudformation.git
 git commit -am "Updating tags to deploy"
 git push --set-upstream origin master
