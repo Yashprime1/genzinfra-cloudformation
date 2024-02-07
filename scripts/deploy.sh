6cb8afc566ca173f1d04200793e4ec42a8ab2883
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 17:26:15 +0530

    updating build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $bamboo_deploy_environment
 echo $bamboo_buildResultKey
-python3 scripts/replace_tags.py
+python3 genzinfra-cloudformation/scripts/replace_tags.py
 git --version
 git config --global user.name "Yashdeep Shetty"
 git config --global user.email yashdeep@clevertap.com
