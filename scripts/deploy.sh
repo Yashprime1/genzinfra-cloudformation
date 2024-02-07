f737e981e6e29e013890110381cd71b06032ff12
Yashdeep Shetty <yashdeep@clevertap.com>
2024-02-07 14:25:10 +0530

    updating build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $bamboo_deploy_environment
 echo $bamboo_buildResultKey
-python3 scripts/replace_tags.py
-git commit -am "Updating tags to deploy"
-git remote add origin https://github.com/Yashprime1/genzinfra-cloudformation.git
-git push --set-upstream origin master
+# python3 scripts/replace_tags.py
+# git commit -am "Updating tags to deploy"
+# git remote add origin https://github.com/Yashprime1/genzinfra-cloudformation.git
+# git push --set-upstream origin master
