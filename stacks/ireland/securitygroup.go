7376faed5c4b3c1332d90914e5e3d36ff61b037b
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2024-12-01 17:10:31 +0530

    aws: Adding core stacks in eu

---
+++ stacks/ireland/securitygroup.go
@@ -0,0 +1,22 @@
+package mumbai
+
+import (
+	"fmt"
+	securitygroup "github.com/Yashprime1/genzinfra-cloudformation/lib/components/core/security-group"
+	utils "github.com/Yashprime1/genzinfra-cloudformation/lib/utilities"
+)
+
+func GenerateSecurityGroupStack() {
+	// Create the stacks
+	fmt.Println("Generating Ireland Security Group stack template")
+
+	// Generate the stack template
+	SecurityGroupTemplate := securitygroup.GenerateCoreSecurityGroupStack("Eu-Network")	
+	//Write the Json Templates
+	SecurityGroupJsonTemplate, err := SecurityGroupTemplate.JSON()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		utils.WriteTemplatesToFile("./templates/ireland/core","ireland-securitygroup.json", SecurityGroupJsonTemplate)
+	}
+}
